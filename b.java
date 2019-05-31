3159eb92f017545e6f1d5ea8bb26e06530817642
xiaohaozi1212 <chenyonghao1212@gmail.com>
2019-05-31 12:36:59 -0400

    Changes in b.java

--- b.java
+++ b.java
@@ -2,6 +2,6 @@ package ca.myseneca.model;
 
 public class b {
 	public static void main(String[] args) throws Exception {
-		System.out.println("input 2st number : ");
+		System.out.println("input 2st number is : ");
 	}
 }
